efe84958d3d1086555f9499a808a969600674faa
Farad078 <faradokorie@gmail.com>
2022-10-18 00:37:53 +0100

    Task-0

--- 0-printf.c
+++ 0-printf.c
@@ -1,6 +1,84 @@
 #include "main.h"
 
 /**
+ * check_sc - check here
+ * Description: check for a string or character.
+ * @x: input to character
+ * @i: input integer
+ * @args: input va_list argument
+ * Return: returns Nothing
+ */
+
+void check_sc(const char *x, int i, va_list args)
+{
+	int k;
+
+	if (x[i] == '%')
+	{
+		k = i;
+		k++;
+		if (x[k] == 'c')
+		{
+			char z = va_arg(args, int);
+
+			printf("%c", z);
+			k = 0;
+		}
+	}
+	if (x[i] == '%')
+	{
+		k = i;
+		k++;
+		if (x[k] == 's')
+		{
+			char *y = va_arg(args, char *);
+
+			printf("%s", y);
+			k = 0;
+		}
+	}
+}
+
+/**
+ * check_id - check here
+ * Description: check for a string or character.
+ * @x: input to character
+ * @i: input integer
+ * @args: input va_list argument
+ * Return: returns Nothing
+ */
+
+void check_id(const char *x, int i, va_list args)
+{
+	int k;
+
+	if (x[i] == '%')
+	{
+		k = i;
+		k++;
+		if (x[k] == 'd')
+		{
+			int y = va_arg(args, int);
+
+			printf("%d", y);
+			k = 0;
+		}
+	}
+	if (x[i] == '%')
+	{
+		k = i;
+		k++;
+		if (x[k] == 'i')
+		{
+			int y = va_arg(args, int);
+
+			printf("%i", y);
+			k = 0;
+		}
+	}
+}
+
+/**
  * _printf - check main
  * Description: a function that produces output according to a format.
  * @format: a pointer to character
@@ -31,50 +109,8 @@ int _printf(const char *format, ...)
 			else
 				continue;
 		}
-		if (format[i] == '%')
-		{
-			k = i;
-			k++;
-			if (format[k] == 'c')
-			{
-				char z = va_arg(args, int);
-				printf("%c", z);
-				k = 0;
-			}
-		}
-		if (format[i] == '%')
-		{
-			k = i;
-			k++;
-			if (format[k] == 's')
-			{
-				x = va_arg(args, char *);
-				printf("%s", x);
-				k = 0;
-			}
-		}
-		if (format[i] == '%')
-		{
-			k = i;
-			k++;
-			if (format[k] == 'd')
-			{
-				int y = va_arg(args, int);
-				printf("%d", y);
-				k = 0;
-			}
-		}
-		if (format[i] == '%')
-		{
-			k = i;
-			k++;
-			if (format[k] == 'i')
-			{
-				int y = va_arg(args, int);
-				printf("%i", y);
-				k = 0;
-			}
-		}
+		check_sc(format, i, args);
+		check_id(format, i, args);
 	}
-	return (i);
+	return (num_arg);
 }
